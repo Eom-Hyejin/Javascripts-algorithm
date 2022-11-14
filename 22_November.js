5b258a836df5a9f9e638d9a1a962ab8e36ea3cb3
Eom-Hyejin <qnwkdhk9192@gmail.com>
2022-11-14 17:58:19 +0900

    November 14_practice

--- 22_November.js
+++ 22_November.js
@@ -19,9 +19,43 @@ console.log(diamond(2), null)
 
 
 function parse (data) {
-
+  let arr = data.split(""), result = [], count = 0
+  for(let i = 0; i < arr.length; i++) { 
+    if(arr[i] === 'i') count++
+    else if(arr[i] === 'd') count--
+    else if(arr[i] === 's') count = Math.pow(count, 2)
+    else if(arr[i] === 'o') result.push(count) 
+  }
+  return result
 }
 
 
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
 console.log(parse("iiisdoso"), [ 8, 64 ])
 console.log(parse("iiisxxxdoso"), [ 8, 64 ])
+
+
+
+function partsSums(ls) {
+  if(!ls.length) return [0]
+    let result = []
+    let count = 0
+    while(ls.length) {
+      for(let i = 0; i < ls.length; i++) {
+        count += ls[i]
+      }
+      result.push(count)
+      ls.shift()
+      count = 0
+    }
+    result[result.length] = 0
+    return result
+}
+
+
+console.log(partsSums([]))
+console.log(partsSums([0, 1, 3, 6, 10]), [20, 20, 19, 16, 10, 0])
+console.log(partsSums([6710213, 2579358, 744125, 935, 889, 810, 454, 430, 407, 144, 90]))
